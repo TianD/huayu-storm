fd6bd7a182bc3a386aa86879baeba9b99bc15455
kiop <nick028@foxmail.com>
2020-03-20 22:10:20 +0800

    update: extend page width

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import { Layout, Menu } from 'antd';
+import {Layout, Menu} from 'antd';
 import Overview from './components/Overview';
 import Logo from './logo.png';
 
-const { Header, Content, Footer } = Layout;
+const {Header, Content, Footer} = Layout;
 
 class App extends Component {
 
@@ -17,7 +17,7 @@ class App extends Component {
 
     onChange(event) {
         this.setState(
-            { page: event.key }
+            {page: event.key}
         )
     }
 
@@ -26,47 +26,45 @@ class App extends Component {
         let current_page;
         switch (current_key) {
             case "1":
-                current_page = <Overview />
+                current_page = <Overview/>
                 break
             case "2":
-                current_page = <div />
+                current_page = <div/>
                 break
             case "3":
-                current_page = <div />
+                current_page = <div/>
                 break
             case "4":
-                current_page = <div />
+                current_page = <div/>
                 break
         }
 
         return (
             <div className="App">
-                <header className="App-header">
-                    <Layout className="layout">
-                        <Header>
-                            <div className="logo" />
+                <Layout className="layout">
+                    <Header>
+                        <div className="logo"/>
 
-                            <Menu
-                                theme="dark"
-                                mode="horizontal"
-                                defaultSelectedKeys={[current_key]}
-                                style={{ lineHeight: '64px' }}
-                                onClick={(value) => this.onChange(value)}
-                            >
-                                <Menu.Item key="1">Compositing</Menu.Item>
-                                <Menu.Item key="2">Maya批处理</Menu.Item>
-                                <Menu.Item key="3">Nuke模板</Menu.Item>
-                                <Menu.Item key="4">序列转视频</Menu.Item>
-                            </Menu>
-                        </Header>
-                        <Content style={{ padding: '0 50px' }}>
-                            <div className="site-layout-content">
-                                {current_page}
-                            </div>
-                        </Content>
-                        <Footer style={{ textAlign: 'center' }}><img src={Logo} height="10%" width="10%" alt="" /></Footer>
-                    </Layout>
-                </header>
+                        <Menu
+                            theme="dark"
+                            mode="horizontal"
+                            defaultSelectedKeys={[current_key]}
+                            style={{lineHeight: '64px'}}
+                            onClick={(value) => this.onChange(value)}
+                        >
+                            <Menu.Item key="1">Compositing</Menu.Item>
+                            <Menu.Item key="2">Maya批处理</Menu.Item>
+                            <Menu.Item key="3">Nuke模板</Menu.Item>
+                            <Menu.Item key="4">序列转视频</Menu.Item>
+                        </Menu>
+                    </Header>
+                    <Content style={{padding: '20px 50px'}}>
+                        <div className="site-layout-content">
+                            {current_page}
+                        </div>
+                    </Content>
+                    <Footer style={{textAlign: 'center'}}><img src={Logo} height="10%" width="10%" alt=""/></Footer>
+                </Layout>
             </div>
         );
     }

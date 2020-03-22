8aa62005c5a9889066e4e0e00e4ce899544a98ca
kiop <nick028@foxmail.com>
2020-03-23 00:08:14 +0800

    update: get preview with md5 name

--- engine/libs/utils.py
+++ engine/libs/utils.py
@@ -1,3 +1,4 @@
+import hashlib
 import os
 import subprocess
 
@@ -20,6 +21,15 @@ def get_file_new_ext_path(file_path, new_ext):
     return '.'.join(file_path_list)
 
 
+def get_file_new_path_with_new_dir_and_new_ext(file_path, new_dir, new_ext, use_md5_name=False):
+    if use_md5_name:
+        new_file_base_name = '{}.{}'.format(get_string_hash(file_path), new_ext)
+    else:
+        new_file_name = get_file_new_ext_path(file_path, new_ext)
+        new_file_base_name = os.path.basename(new_file_name)
+    return os.path.join(new_dir, new_file_base_name)
+
+
 def get_file_abs_path(file_path):
     return os.path.abspath(file_path)
 
@@ -39,29 +49,39 @@ def is_file_exits(file_path):
     return os.path.isfile(file_path)
 
 
-def get_preview_cache_path(origin_image_path):
-    NEW_FORMAT_EXT = 'preview.jpg'
+def get_string_hash(string):
+    md5 = hashlib.md5()
+    md5.update(string)
+    return md5.hexdigest()
 
-    preview_path = get_file_new_ext_path(origin_image_path, NEW_FORMAT_EXT)
+
+def get_preview_cache_path(origin_image_path):
+    NEW_FORMAT_EXT = 'jpg'
+    CACHE_PATH = '../../cache_dir'
+    PREVIEW_WIDTH = 640
+
+    preview_path = \
+        get_file_new_path_with_new_dir_and_new_ext(origin_image_path, CACHE_PATH, NEW_FORMAT_EXT, use_md5_name=True)
     ensure_file_dir_exists(preview_path)
 
     if not is_file_exits(preview_path):
         converter_bin = get_sibling_file_path(__file__, '../../bin/ffmpeg.exe')
-        preview_width = 640
 
         command_arg_dict = {
             'ffmpeg_bin': get_file_native_abs_path(converter_bin),
             'origin_image_path': get_file_native_abs_path(origin_image_path),
-            'preview_width': preview_width,
+            'preview_width': PREVIEW_WIDTH,
             'preview_path': get_file_native_abs_path(preview_path),
         }
         CONVERT_COMMAND = \
-            '"{ffmpeg_bin}" -i "{origin_image_path}" -vs scale={preview_width}:-1 "{preview_path}"'.format(
+            '"{ffmpeg_bin}" -i "{origin_image_path}" -vf scale={preview_width}:-1 "{preview_path}"'.format(
                 **command_arg_dict)
-        child = subprocess.Popen(CONVERT_COMMAND, shell=True)
+        child = subprocess.Popen(CONVERT_COMMAND, shell=False)
         child.wait()
     return preview_path
 
 
 if __name__ == '__main__':
-    get_preview_cache_path(r'E:\codeLib\___test___\my_proj\huayu_project\huayu-storm\examples\huayu-storm\TTT\compositing\EP01\Q01\S01\ttt_EP01_Q01_S01_cp_c001.1002.jpg')
+    get_preview_cache_path(
+        r'E:\codeLib\___test___\my_proj\huayu_project\huayu-storm\examples\huayu-storm\TTT\compositing\EP01\Q01\S01\ttt_EP01_Q01_S01_cp_c001.1002.jpg'
+    )

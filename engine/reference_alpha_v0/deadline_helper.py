acebeab37728779b841698a26ac94bd8edd5308b
kiop <nick028@foxmail.com>
2020-05-07 09:13:46 +0800

    update: add deadline parameter

--- engine/reference_alpha_v0/deadline_helper.py
+++ engine/reference_alpha_v0/deadline_helper.py
@@ -1,3 +1,41 @@
 # coding=utf8
 from __future__ import absolute_import
 
+deadline_parameter_dict = {
+    'scene_name': '',
+    'scene_file_path': '',
+    'project_dir': '',
+    'maya_version': '',  # 2017
+    'submit_user_name': 'python_submitter',
+    'frame_range': '1-129',
+    'machine_name': 'machine',
+    'output_dir': 'z:/',
+}
+
+# jobInfo.job
+job_info_string = """
+Name={scene_name}
+UserName={submit_user_name}
+Frames={frame_range}
+MachineName={machine_name}
+Plugin=MayaBatch
+OutputDirectory0={output_dir}
+"""
+
+# plugInfo.job
+plugin_info_string = """
+SceneFile={scene_file_path}
+Version={maya_version}
+Build=64bit
+ProjectPath={project_dir}
+StrictErrorChecking=True
+LocalRendering=False
+MaxProcessors=0
+FrameNumberOffset=0
+OutputFilePath={output_dir}
+Renderer=File
+StartupScript=
+CommandLineOptions=
+UseOnlyCommandLineOptions=0
+IgnoreError211=False
+"""
